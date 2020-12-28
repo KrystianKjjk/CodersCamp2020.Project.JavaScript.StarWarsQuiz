6b0a3d3265732f9d3f3b5faa28cefe01071d3725
Robert <robert.czwartosz@tlen.pl>
2020-12-28 12:49:35 +0100

    throw error if string is not passed

--- src/app/PeopleImageToRecognize.js
+++ src/app/PeopleImageToRecognize.js
@@ -1,4 +1,5 @@
 function peopleImageToRecognize(base64) {
+    if (typeof base64 !== 'string') throw Error('Pass base64 string as function argument');
     const img = document.createElement('img');
     img.src = `data:image/png;base64,${base64}`;
     img.alt = `obraz People`;
