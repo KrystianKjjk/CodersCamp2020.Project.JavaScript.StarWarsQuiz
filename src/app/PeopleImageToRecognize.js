00b654ef533ff3c2566d727ce33db2958b88ed25
Robert <robert.czwartosz@tlen.pl>
2020-12-28 12:32:44 +0100

    change names

--- src/app/PeopleImageToRecognize.js
+++ src/app/PeopleImageToRecognize.js
@@ -1,6 +1,6 @@
-function peopleObrazDoRozpoznania(obraz) {
+function peopleImageToRecognize(base64) {
     const img = document.createElement('img');
-    img.src = `data:image/png;base64,${obraz}`;
+    img.src = `data:image/png;base64,${base64}`;
     img.alt = `obraz People`;
     return img;
 }
