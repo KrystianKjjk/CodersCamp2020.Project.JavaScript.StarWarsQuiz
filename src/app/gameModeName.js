f1cc3dc1bc3b028ecfb5496b595b2581656993f5
FIlip <filip.szwedo@gmail.com>
2021-01-01 19:24:07 +0100

    code review fixes

--- src/app/gameModeName.js
+++ src/app/gameModeName.js
@@ -4,19 +4,15 @@ const createGameModeName = (modeName) => {
     
     switch (modeName) {
         case 'People':
-            gameModeNameDiv.id = 'people';
             gameModeNameDiv.textContent = 'Who is this character?';
             break;
         case 'Vehicles':
-            gameModeNameDiv.id = 'vehicles';
             gameModeNameDiv.textContent = 'Do you recognize this vehicle?';
             break;
         case 'Starships':
-            gameModeNameDiv.id = 'starships';
             gameModeNameDiv.textContent = 'Do you recognize this starship?';
             break;
         default:
-            gameModeNameDiv.id = 'unknown';
             gameModeNameDiv.textContent = 'Unknown game mode';
             throw 'Mode name argument was not passed to the createGameModeName component!'
     }
