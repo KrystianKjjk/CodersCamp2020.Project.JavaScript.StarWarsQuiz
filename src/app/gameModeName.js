4ef15f61d972a44b2d0a28221358d0a50850bdfc
FIlip <filip.szwedo@gmail.com>
2021-01-01 18:23:59 +0100

    component + tests written

---
+++ src/app/gameModeName.js
@@ -0,0 +1,27 @@
+const createGameModeName = (modeName) => {
+    let gameModeNameDiv = document.createElement('div');
+    gameModeNameDiv.className = 'gameModeContainer';
+    
+    switch (modeName) {
+        case 'People':
+            gameModeNameDiv.id = 'people';
+            gameModeNameDiv.textContent = 'Who is this character?';
+            break;
+        case 'Vehicles':
+            gameModeNameDiv.id = 'vehicles';
+            gameModeNameDiv.textContent = 'Do you recognize this vehicle?';
+            break;
+        case 'Starships':
+            gameModeNameDiv.id = 'starships';
+            gameModeNameDiv.textContent = 'Do you recognize this starship?';
+            break;
+        default:
+            gameModeNameDiv.id = 'unknown';
+            gameModeNameDiv.textContent = 'Unknown game mode';
+            throw 'Mode name argument was not passed to the createGameModeName component!'
+    }
+
+    document.body.appendChild(gameModeNameDiv); 
+}
+
+export default createGameModeName;
