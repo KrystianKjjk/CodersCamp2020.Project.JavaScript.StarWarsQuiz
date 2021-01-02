fcef983c5e2a932fbcdf0f21bbaf890caa8d4907
Konrad <kj.reczek@gmail.com>
2021-01-02 19:32:33 +0100

    create separate style files and add onclick functions

--- src/app/redButton.js
+++ src/app/redButton.js
@@ -1,11 +1,18 @@
-function redButton(label) {
+function redButton(label, onClick) {
     if (typeof label !== 'string') {
-      throw Error('Label is not string.');
+      throw Error('Label should be a string.');
+    }
+
+    if (typeof onClick !== 'function'){
+      throw Error('OnClick should be a function.');
     }
     
     const button = document.createElement('button');
-    button.className = 'red-button';
+    button.className = 'button red-button';
     button.innerText = label;
+    button.addEventListener('click', (e) => {
+      onClick();
+    }); 
     
     return button;
   }
