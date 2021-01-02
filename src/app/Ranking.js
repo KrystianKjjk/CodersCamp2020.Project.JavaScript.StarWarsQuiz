24cfa7870cb39a34d623893b9cbc0b45aa003115
Patryqss <julia.goat@gmail.com>
2021-01-02 15:02:19 +0100

    Add icon to Ranking title

--- src/app/Ranking.js
+++ src/app/Ranking.js
@@ -7,11 +7,13 @@ function displayRanking(listOfUsers) {
   ranking.id = 'theBestPlayers';
 
   const titleDiv = document.createElement('div');
+  const image = document.createElement('i');
+  image.className = 'fas fa-id-card-alt';
   const title = document.createElement('p');
-  //TO DO: upload image
   titleDiv.className = 'rankingTitle';
   title.className = 'rankingTitle';
   title.innerText = 'Mode Ranking';
+  titleDiv.appendChild(image);
   titleDiv.appendChild(title);
   ranking.appendChild(titleDiv);
 
