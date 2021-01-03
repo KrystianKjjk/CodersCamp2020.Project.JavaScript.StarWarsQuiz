9b3fb0005a9a5092895be0dccb5a121262928344
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-04 00:32:29 +0100

    Moved from then to async await, create tests for Starships and Vehicles Mode

--- src/QuestionsGenerator.js
+++ src/QuestionsGenerator.js
@@ -1,10 +1,11 @@
+import { async } from "regenerator-runtime";
 import { GameModes } from "./Consts";
 import { getPersonById, getPersonImageBlobById } from "./PeopleClient";
 import { getStarshipById, getStarshipImageBlobById } from "./StarshipsClient";
 import { getVehicleById, getVehicleImageBlobById } from "./VehiclesClient";
 
 
-function generateQuestions(gameMode) {
+async function generateQuestions(gameMode) {
     if (gameMode === GameModes.PEOPLE) {
         const allowedIds = [];
         for (let i = 1; i < 83; i++) {
@@ -13,83 +14,75 @@ function generateQuestions(gameMode) {
         }
 
         const questionsIds = drawQuestionsIds(allowedIds);
-        return Promise.all([
-                getPersonById(questionsIds[0]),
-                getPersonById(questionsIds[1]),
-                getPersonById(questionsIds[2]),
-                getPersonById(questionsIds[3])
-            ])
-            .then(peopleArray => {
-                const peopleNamesArray = peopleArray.map(person => person.name);
-                const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, peopleNamesArray);
+        const peopleArray = await Promise.all([
+            getPersonById(questionsIds[0]),
+            getPersonById(questionsIds[1]),
+            getPersonById(questionsIds[2]),
+            getPersonById(questionsIds[3])
+        ]);
+        const peopleNamesArray = peopleArray.map(person => person.name);
+        const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, peopleNamesArray);
 
-                const questionObject = {
-                    image: '',
-                    answers: peopleNamesArray,
-                    rightAnswer: correctAnswer,
-                }
-                return getPersonImageBlobById(correctAnswerId)
-                    .then(convertBlobToBase64)
-                    .then(base64Image => {
-                        questionObject.image = base64Image;
-                        return questionObject;
-                    })
-            });
+        const questionObject = {
+            image: '',
+            answers: peopleNamesArray,
+            rightAnswer: correctAnswer,
+        }
 
+        const imageBlob = await getPersonImageBlobById(correctAnswerId);
+        const imageBase64 = await convertBlobToBase64(imageBlob);
+        questionObject.image = imageBase64;
+        return questionObject;
 
-
-    } else if (gameMode === GameModes.STARCHIPS) {
+    } else if (gameMode === GameModes.STARSHIPS) {
         const allowedIds = [5, 9, 10, 11, 12, 13, 15, 21, 22, 23, 27, 28, 29, 31, 39, 40, 41, 43, 47, 48];
 
         const questionsIds = drawQuestionsIds(allowedIds);
-        return Promise.all([
-                getStarshipById(questionsIds[0]),
-                getStarshipById(questionsIds[1]),
-                getStarshipById(questionsIds[2]),
-                getStarshipById(questionsIds[3])
-            ])
-            .then(starshipsArray => {
-                const starshipsNamesArray = starshipsArray.map(starship => starship.name);
-                const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, starshipsNamesArray);
+        const starshipsArray = await Promise.all([
+            getStarshipById(questionsIds[0]),
+            getStarshipById(questionsIds[1]),
+            getStarshipById(questionsIds[2]),
+            getStarshipById(questionsIds[3])
+        ])
 
-                const questionObject = {
-                    image: '',
-                    answers: starshipsNamesArray,
-                    rightAnswer: correctAnswer,
-                }
-                return getStarshipImageBlobById(correctAnswerId)
-                    .then(convertBlobToBase64)
-                    .then(base64Image => {
-                        questionObject.image = base64Image;
-                        return questionObject;
-                    })
-            });
+        const starshipsNamesArray = starshipsArray.map(starship => starship.name);
+        const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, starshipsNamesArray);
+
+        const questionObject = {
+            image: '',
+            answers: starshipsNamesArray,
+            rightAnswer: correctAnswer,
+        }
+
+        const imageBlob = await getStarshipImageBlobById(correctAnswerId);
+        const imageBase64 = await convertBlobToBase64(imageBlob);
+        questionObject.image = imageBase64;
+        return questionObject;
+
+
     } else if (gameMode === GameModes.VEHICLES) {
         const allowedIds = [4, 6, 7, 8, 14, 16, 18, 20, 24, 25, 26, 30, 33, 34, 35, 36, 37, 38, 42];
 
         const questionsIds = drawQuestionsIds(allowedIds);
-        return Promise.all([
-                getVehicleById(questionsIds[0]),
-                getVehicleById(questionsIds[1]),
-                getVehicleById(questionsIds[2]),
-                getVehicleById(questionsIds[3])
-            ])
-            .then(vehiclesArray => {
-                const vehiclesNamesArray = vehiclesArray.map(vehicle => vehicle.name);
-                const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, vehiclesNamesArray);
+        const vehiclesArray = await Promise.all([
+            getVehicleById(questionsIds[0]),
+            getVehicleById(questionsIds[1]),
+            getVehicleById(questionsIds[2]),
+            getVehicleById(questionsIds[3])
+        ])
 
-                const questionObject = {
-                    image: '',
-                    answers: vehiclesNamesArray,
-                    rightAnswer: correctAnswer,
-                }
-                return getVehicleImageBlobById(correctAnswerId)
-                    .then(convertBlobToBase64)
-                    .then(base64Image => {
-                        questionObject.image = base64Image;
-                        return questionObject;
-                    })
-            });
+        const vehiclesNamesArray = vehiclesArray.map(vehicle => vehicle.name);
+        const { correctAnswer, correctAnswerId } = drawCorrectAnswer(questionsIds, vehiclesNamesArray);
+
+        const questionObject = {
+            image: '',
+            answers: vehiclesNamesArray,
+            rightAnswer: correctAnswer,
+        }
+        const imageBlob = await getVehicleImageBlobById(correctAnswerId);
+        const imageBase64 = await convertBlobToBase64(imageBlob);
+        questionObject.image = imageBase64;
+        return questionObject;
 
     } else {
         throw new Error('Invalid game mode');
--- test/QuestionsGenerator.test.js
+++ test/QuestionsGenerator.test.js
@@ -1,22 +1,32 @@
 import generateQuestions from '../src/QuestionsGenerator';
 import * as PeopleClient from '../src/PeopleClient';
+import * as StarshipsClient from '../src/StarshipsClient';
+import * as VehiclesClient from '../src/VehiclesClient';
+import { GameModes } from '../src/Consts';
 
 const mockBlob = new Blob(["Hello, world!"], { type: 'text/plain' });
 
 describe('Questions Generator tests', () => {
+    beforeEach(() => {
+        jest.spyOn(global.Math, "random");
+    })
+
+    afterEach(() => {
+        jest.spyOn(global.Math, 'random').mockRestore();
+    })
+
     test('should throw error when invalid game mode', () => {
-        expect(() => generateQuestions('invalidGameMode')).toThrow('Invalid game mode');
+        expect(generateQuestions('invalidGameMode')).rejects.toEqual(TypeError('Invalid game mode'));
     })
 
     test('should generate questions for people game mode', async() => {
-        jest.spyOn(global.Math, "random");
         jest.spyOn(PeopleClient, "getPersonById").mockResolvedValueOnce({ name: "Barriss Offee" })
             .mockResolvedValueOnce({ name: "Obi-Wan Kenobi" })
             .mockResolvedValueOnce({ name: "Ratts Tyerel" })
             .mockResolvedValueOnce({ name: "Jocasta Nu" });
         jest.spyOn(PeopleClient, "getPersonImageBlobById").mockResolvedValueOnce(mockBlob);
 
-        const questionObject = await generateQuestions("People");
+        const questionObject = await generateQuestions(GameModes.PEOPLE);
         expect(questionObject.answers.length).toBe(4); //Pytanie zawsze musi miec 4 odpowiedzi
         expect(questionObject.answers.includes(questionObject.rightAnswer)).toBeTruthy(); //Prawidlowa odpowiedz musi byc jedna z wylosowanych odpowiedzi
         expect(questionObject.image).toBeDefined(); //Image musi byc obecny
@@ -25,36 +35,37 @@ describe('Questions Generator tests', () => {
         expect(PeopleClient.getPersonImageBlobById).toHaveBeenCalledTimes(1);
     })
 
-    // Dzieki zamockowaniu Math.random mozemy sprawdzic, czy getPersonById i getPersonImageBlobById jest wywolane z odpowiednimi wartosciami, 
-    // jednakze jednoczesnie wedlug mnie ten test dotyka juz za bardzo implementacji. Dlatego osobiscie chyba bym zrezygnowal z mockowania tego
-    // i pozostal przy samym sprawdzeniu, ze Math.random zostalo wywolane 5 razy (4 razy przy pierwszym losowaniu i raz przy drugim).
+    test('should generate questions for starships game mode', async() => {
+        jest.spyOn(StarshipsClient, "getStarshipById").mockResolvedValueOnce({ name: "A-wing" })
+            .mockResolvedValueOnce({ name: "Imperial shuttle" })
+            .mockResolvedValueOnce({ name: "B-wing" })
+            .mockResolvedValueOnce({ name: "Calamari Cruiser" });
+        jest.spyOn(StarshipsClient, "getStarshipImageBlobById").mockResolvedValueOnce(mockBlob);
 
-    // test('should generate questions for people game mode', async() => {
-    //     jest.spyOn(global.Math, "random").mockReturnValue(0);
-    //     jest.spyOn(PeopleClient, "getPeople").mockResolvedValue({ count: 82 });
-    //     jest.spyOn(PeopleClient, "getPersonById").mockResolvedValueOnce({ name: "Barriss Offee" })
-    //         .mockResolvedValueOnce({ name: "Obi-Wan Kenobi" })
-    //         .mockResolvedValueOnce({ name: "Ratts Tyerel" })
-    //         .mockResolvedValueOnce({ name: "Jocasta Nu" });
-    //     jest.spyOn(PeopleClient, "getPersonImageBlobById").mockResolvedValueOnce(mockBlob);
-
-    //     const questionObject = await generateQuestions("People");
-    //     expect(questionObject.answers.length).toBe(4);
-    //     expect(questionObject.answers.includes(questionObject.rightAnswer)).toBeTruthy();
-    //     expect(questionObject.image).toBeDefined();
-    //     expect(global.Math.random).toHaveBeenCalledTimes(5);
-    //     expect(PeopleClient.getPersonById).toHaveBeenNthCalledWith(1, 1);
-    //     expect(PeopleClient.getPersonById).toHaveBeenNthCalledWith(2, 2);
-    //     expect(PeopleClient.getPersonById).toHaveBeenNthCalledWith(3, 3);
-    //     expect(PeopleClient.getPersonById).toHaveBeenNthCalledWith(4, 4);
-    //     expect(PeopleClient.getPersonImageBlobById).toHaveBeenNthCalledWith(1, 1);
-    // })
-
-    test('should generate questions for starships game mode', () => {
-
+        const questionObject = await generateQuestions(GameModes.STARSHIPS);
+        expect(questionObject.answers.length).toBe(4);
+        expect(questionObject.answers.includes(questionObject.rightAnswer)).toBeTruthy();
+        expect(questionObject.image).toBeDefined();
+        expect(global.Math.random).toHaveBeenCalledTimes(5);
+        expect(StarshipsClient.getStarshipById).toHaveBeenCalledTimes(4);
+        expect(StarshipsClient.getStarshipImageBlobById).toHaveBeenCalledTimes(1);
     })
 
-    test('should generate questions for vehicles game mode', () => {
+    test('should generate questions for vehicles game mode', async() => {
+        jest.spyOn(VehiclesClient, "getVehicleById").mockResolvedValueOnce({ name: "Multi-Troop Transport" })
+            .mockResolvedValueOnce({ name: "Vulture Droid" })
+            .mockResolvedValueOnce({ name: "C-9979 landing craft" })
+            .mockResolvedValueOnce({ name: "X-34 landspeeder" });
+        jest.spyOn(VehiclesClient, "getVehicleImageBlobById").mockResolvedValueOnce(mockBlob);
+
+        const questionObject = await generateQuestions(GameModes.VEHICLES);
+        expect(questionObject.answers.length).toBe(4);
+        expect(questionObject.answers.includes(questionObject.rightAnswer)).toBeTruthy();
+        expect(questionObject.image).toBeDefined();
+        expect(global.Math.random).toHaveBeenCalledTimes(5);
+        expect(VehiclesClient.getVehicleById).toHaveBeenCalledTimes(4);
+        expect(VehiclesClient.getVehicleImageBlobById).toHaveBeenCalledTimes(1);
+
 
     })
 
