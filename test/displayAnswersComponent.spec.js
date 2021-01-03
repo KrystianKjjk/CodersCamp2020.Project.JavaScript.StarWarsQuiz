7ea2bad8196491a3f8b81373e5e6821f51686976
ireneusz <ireneusz.petryszak@gmail.com>
2021-01-03 02:36:48 +0100

    added comments, got rid off unclear numerals, edited solution, added tests

--- test/displayAnswersComponent.spec.js
+++ test/displayAnswersComponent.spec.js
@@ -15,16 +15,16 @@ describe('displayAnswersComponent test', () => {
   });
   it('Valid number of buttons', () => {
     expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
-    .not.toThrow(new Error('Invalid number of buttons.'));
+    .not.toThrowError(new Error('Invalid number of buttons.'));
   });
   it('Valid number of possible answers', () => {
     expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
-    .not.toThrow(new Error('Invalid number of possible answers'));
+    .not.toThrowError(new Error('Invalid number of possible answers'));
   });
   it('Invalid number of possible answers', () => {
     const possibleAnswers = ['Luke Skywalker', 'Jar Jar Binksa', 'Padme Amidala'];
     expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
-      .toThrow(new Error('Invalid number of answers'));
+      .toThrowError(new Error('Invalid number of answers'));
   });
   it('Possible answers not passed', () => {
     const possibleAnswers = [];
@@ -35,5 +35,15 @@ describe('displayAnswersComponent test', () => {
     const correctAnswer = '';
     expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
       .toThrowError(new Error('Correct answer not passed'));
+  });
+  it('Possible Answers are not strings', () => {
+    const possibleAnswers = [1,2,'Luke Skywalker', 'Jar Jar Binksa'];
+    expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
+      .toThrowError(new Error('Possible answers must be strings'));
+  });
+  it('Correct answer must be string', () => {
+    const correctAnswer = 2;
+    expect(() => displayAnswersComponent(possibleAnswers,correctAnswer))
+      .toThrowError(new Error('Correct answer must be a string'));
   });
 });
