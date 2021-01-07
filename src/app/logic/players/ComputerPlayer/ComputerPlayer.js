86cf94aa1161d3d1b404ee923e60660bb844a252
Konrad <kj.reczek@gmail.com>
2021-01-07 22:09:30 +0100

    Create computer player

---
+++ src/app/logic/players/ComputerPlayer/ComputerPlayer.js
@@ -0,0 +1,24 @@
+function createPlayer() {
+    const player = {
+        giveAnswerCallbacks = [],
+
+        onGiveAnswerDo(callback) {
+            this.giveAnswerCallbacks.push(callback);
+        },
+
+        answerQuestion(question) {
+            const answer = this._giveRandomAnswer(question.answers);
+            this.giveAnswerCallbacks.forEach((callback) => {callback(answer, question)});
+
+            return answer;
+        },
+
+        _giveRandomAnswer(answers) {
+            const randomIndex = Math.floor(Math.random() * answers.length);
+
+            return answers[randomIndex];
+        }
+    };
+    
+    return player;
+} 
