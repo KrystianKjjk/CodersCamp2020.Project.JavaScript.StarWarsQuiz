cfb8a4b68e98b48c3fbe35cf721922f9b0897221
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-11 00:07:36 +0100

    Added lightsaber logic. HTML generated in function

--- src/app/App.js
+++ src/app/App.js
@@ -3,5 +3,5 @@ import { generateTextTimerComponent } from "./uicomponents/TextTimer/TextTimer";
 import { generateLightsaberTimerComponent } from './uicomponents/LightsaberTimer/LightsaberTimer';
 export const App = ({ options }) => {
 
-    // const div = document.getElementById('swquiz-app').appendChild(generateLightsaberTimerComponent());
+    document.getElementById('swquiz-app').appendChild(generateLightsaberTimerComponent());
 };
--- src/app/uicomponents/LightsaberTimer/LightsaberTimer.js
+++ src/app/uicomponents/LightsaberTimer/LightsaberTimer.js
@@ -1,8 +1,36 @@
 function generateLightsaberTimerComponent() {
-    const div = document.createElement('div');
-    div.className = 'lightsaber';
-    div.innerHTML = 'ABC';
-    return div;
+
+    const lightsaberDiv = document.createElement('div');
+    lightsaberDiv.className = 'lightsaber';
+    const handleDiv = document.createElement('div');
+    handleDiv.className = 'handle';
+    lightsaberDiv.appendChild(handleDiv);
+    const handleImage = document.createElement('img');
+    handleImage.src = "/static/assets/ui/LightsaberHandle.png";
+    handleImage.alt = 'Handle image';
+    handleDiv.appendChild(handleImage);
+    const saberDiv = document.createElement('div');
+    lightsaberDiv.appendChild(saberDiv);
+    saberDiv.className = 'saber';
+    const progressDiv = document.createElement('div');
+    saberDiv.appendChild(progressDiv);
+    progressDiv.className = 'progress-bar';
+
+
+    let begWidth = 100;
+    let gameTime = 120;
+    const totalGameTime = 120;
+
+    let interval = setInterval(() => {
+        gameTime--;
+
+        const width = gameTime * begWidth / totalGameTime;
+
+        progressDiv.style.width = `${width}%`;
+        if (gameTime === 0) { clearInterval(interval); }
+    }, 1000);
+
+    return lightsaberDiv;
 }
 
 export { generateLightsaberTimerComponent };
