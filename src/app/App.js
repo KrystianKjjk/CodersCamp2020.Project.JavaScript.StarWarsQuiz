dd4be00f19dc35050c3e109f917543b57098d1ea
JustynaSmoliga <justyna.smoliga@gmail.com>
2021-01-12 23:51:24 +0100

    Removed changes from index.html and App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,1 @@
-import { generateTextTimerComponent } from "./uicomponents/TextTimer/TextTimer";
-
-import { generateLightsaberTimerComponent } from './uicomponents/LightsaberTimer/LightsaberTimer';
-export const App = ({ options }) => {
-
-    document.getElementById('swquiz-app').appendChild(generateLightsaberTimerComponent());
-};
+export const App = ({ options }) => {};
