c2a60e1b2175f8b38261322c731c612b8da2a79c
fszwedo <74790330+fszwedo@users.noreply.github.com>
2021-01-17 11:47:37 +0100

    Update ConstRulesTexts.js

--- src/app/ConstRulesTexts.js
+++ src/app/ConstRulesTexts.js
@@ -1,7 +1,7 @@
 const RULESTEXTS = {
-    PEOPLERULES: 'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left from available options.',
-    VEHICLESRULES: 'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select which vehicle from Star Wars is showed on the left from available options.',
-    STARSHIPSRULES: 'You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select which starship from Star Wars is showed on the left from available options.',
+    PEOPLERULES: 'You have two minutes (2m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left from available options.',
+    VEHICLESRULES: 'You have two minutes (2m) to answer as many questions as possible. During the game on each question you need to select which vehicle from Star Wars is showed on the left from available options.',
+    STARSHIPSRULES: 'You have two minutes (2m) to answer as many questions as possible. During the game on each question you need to select which starship from Star Wars is showed on the left from available options.',
     DEFAULTRULES: 'Welcome young padawan! Select the game mode using the buttons above.' //for app initialization when no mode is selected
 }
 
